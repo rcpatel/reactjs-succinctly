e00a7bdd022e1073a16946fdad03e6dfba95a986
rcpatel <rcpatel79@gmail.com>
2018-11-14 22:49:11 -0500

    Added src folder

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+import '../styles/App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My React App is here or is it?</h1>
+                <h1>Yes it is!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
